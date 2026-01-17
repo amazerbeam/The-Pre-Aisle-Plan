bf6855dc74c7e4a6bfb3674ecdb82be3170cd3c1
joss.duffy <jossduffy.jd@gmail.com>
2026-01-17 08:27:21 +0000

    Update SecurityConfig.java

--- foodbytes-app/foodbytes-api/src/main/java/com/foodbytes/config/SecurityConfig.java
+++ foodbytes-app/foodbytes-api/src/main/java/com/foodbytes/config/SecurityConfig.java
@@ -97,7 +97,7 @@ public class SecurityConfig {
             "https://resourceful-healing-production-d127.up.railway.app",
             "https://mypantryplan.com"
         ));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
