c9c91c4242ae60f9e4b1b062e383d81128134289
joss.duffy <jossduffy.jd@gmail.com>
2026-04-08 09:34:14 +0100

    Fixed issues with selecting dates

--- foodbytes-app/client/src/utils/dateUtils.js
+++ foodbytes-app/client/src/utils/dateUtils.js
@@ -9,7 +9,10 @@
  * @returns {string}
  */
 export const formatDateISO = (date) => {
-  return date.toISOString().split('T')[0]
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  return `${year}-${month}-${day}`
 }
 
 /**
